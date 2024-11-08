e608e326a9f46e42f8fb789c93980bf495a8deb2
Trevor Lee <49603972+trevortylerlee@users.noreply.github.com>
2024-11-07 16:45:15 -0800

    upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  // change this to your own domain
   site: "https://barebones.superwebthemes.com",
   integrations: [tailwind(), sitemap()],
 });
