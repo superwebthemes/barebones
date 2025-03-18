3a719cd839d5e3ee2aa8b559d25237be4e34ce4d
Trevor Lee <trevortylerlee@gmail.com>
2025-03-18 01:11:34 -0700

    install tailwindcss v4
    - uninstall @astrojs/tailwind
    - install tailwindcss and @tailwindcss/vite
    - delete reset.css
    - delete tailwind.config.mjs
    - remove unnecessary css
    - add prose classes to Introduction, remove <br>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   // change this to your own domain
   site: "https://barebones.superwebthemes.com",
-  integrations: [tailwind(), sitemap()],
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
