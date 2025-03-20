b319920c1b85cfc8ef829f64c615cf0cb3921521
Trevor Lee <trevortylerlee@gmail.com>
2025-03-20 01:48:25 -0700

    add meta colors in <head>, remove unused comments, fixed SeoPage and SeoPost using url instead of href, refactor ThemeToggle to add .light instead of only adding and removing .dark

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  // change this to your own domain
   site: "https://barebones.superwebthemes.com",
   integrations: [sitemap()],
   vite: {
